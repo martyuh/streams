1a40fda173e01984b0561f01abd33ff6336f75b8
Aychtee <aychteeht@gmail.com>
2021-04-22 18:53:11 -0700

    useSelector

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+ import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
 import {fetchStream} from '../../actions'
 import {useSelector,useDispatch} from 'react-redux'
@@ -6,25 +6,25 @@ import {useSelector,useDispatch} from 'react-redux'
 const StreamShow = (props) => {
     const dispatch = useDispatch()
     useEffect(()=>{
-        
+                // component makes a call to the redux store using the action creator in order to set the object to the stream that was selected. without this the stream selected will be undefined because it is making a call to an empty object
+            // usedispatch functions as matchdispatchtoprops
         dispatch(fetchStream(props.match.params.id))
     },[])
-
-       
-        const stream= useSelector(state=>{
-            console.log(state)
-            return state.streams[props.match.params.id]})
-        
+    // useSelector functions as mapstatetoprops by grabbing the state
+    const stream= useSelector(state=>state.streams[props.match.params.id])
+    
+    // if statement can be outside of function
+    if(!stream){
+        // a return statement renders the component jsx can be returned anywhere within a functional component
+        return <div>Loading...</div>
+    }//once redux loads with fetchstream,the component rerenders and stream will be available. that is when destructuring will be possible and stream will be defined and the return statement below will be returned with jsx
+       const {title, description} =stream
        
     return (
-        <div>
-            {!stream?<div>Loading...</div>:
             <div>
-             {/* <h1>{title}</h1>
-            <h5>{description}</h5> */}
-            </div>}
-        </div>
-        
+            <h1>{title}</h1>
+            <h5>{description}</h5>
+            </div>
     )
 }
 
